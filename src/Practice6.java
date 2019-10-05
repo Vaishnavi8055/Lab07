08ef8e9511d7473da3b1a05ba99cb3fc8fa5219d
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 09:27:11 -0700

    changed to uppercase

--- src/Practice6.java
+++ src/Practice6.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Practice6 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-System.out.println("Enter a String");
-String name=scanner.nextLine();
+       System.out.println("Enter a String");
+       String name=scanner.nextLine();
+       System.out.println(name.toUpperCase());
     }
 }
