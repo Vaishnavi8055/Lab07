529e241d374ba97c713c71c539131f60a5760ecb
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 08:02:39 -0700

    Initial commit

---
+++ src/Practice1.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Practice1 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        String inputString=scanner.nextLine();
+    }
+
+}
