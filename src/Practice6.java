2b47b8b527b6980e4e2e55340f903d5195f809cf
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 09:25:14 -0700

    input by user

--- src/Practice6.java
+++ src/Practice6.java
@@ -4,5 +4,6 @@ public class Practice6 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
 System.out.println("Enter a String");
+String name=scanner.nextLine();
     }
 }
