21a81d0f491657866dda43167fc635f3b13ced55
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 10:10:23 -0700

    final comit

--- src/Practice7.java
+++ src/Practice7.java
@@ -6,9 +6,11 @@ public class Practice7 {
         String name = scanner.nextLine();
         char character[] = name.toCharArray();
         int length = name.length();
-        int sum=0;
-        for(int i=0;i<length;i++) {
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            int a = (int) character[i];
+            sum = sum + a;
         }
-
-}
+        System.out.println("Total Weight=" + sum);
+    }
 }
