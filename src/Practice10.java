0b410493347b4d2aca0338cd035e9fb35c09ddf6
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 10:25:10 -0700

    input by user

--- src/Practice10.java
+++ src/Practice10.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 public class Practice10 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-
+        String name=sc.nextLine();
     }
 }
