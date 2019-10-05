c0e2f73efc67b95ff2cf2432bb0836e035df78db
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 08:13:35 -0700

    for loop for chars

--- src/Practice1.java
+++ src/Practice1.java
@@ -7,6 +7,10 @@ public class Practice1 {
         for(int i=0;i<length;i++){
             System.out.print("\t"+i);
         }
+        System.out.print("\n");
+        for(int j=0;j<length;j++){
+            System.out.print("\t"+inputString.charAt(j));
+        }
 
     }
 
