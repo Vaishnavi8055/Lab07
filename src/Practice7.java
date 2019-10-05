8f0b28b5ad1b6861f2a9781ed98a8bc2b9c1c5be
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 10:01:59 -0700

    input string

--- src/Practice7.java
+++ src/Practice7.java
@@ -3,7 +3,15 @@ public class Practice7 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter a String");
-            String name=scanner.nextLine();
+        String name=scanner.nextLine();
+        char character[]=name.toCharArray();
+        int length=name.length();
+        int sum=0;
+        for(int i=0;i<length;i++) {
+            int a = (int) character[i];
+            sum = sum + a;
+        }
+            System.out.print("Total weight="+sum);
     }
 
 }
