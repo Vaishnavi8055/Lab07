731d0479255aeebf3f4083044dabd362ad6d6446
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 10:32:02 -0700

    final comit

--- src/Practice10.java
+++ src/Practice10.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 public class Practice10 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("enter a string");
         String name=sc.nextLine();
+        char c='a';
+        System.out.println(name.indexOf(c));
     }
 }
