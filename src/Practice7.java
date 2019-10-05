6175bb9f94567893877d47b5b9267b8089d0afc7
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 10:04:20 -0700

    input length

--- src/Practice7.java
+++ src/Practice7.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 public class Practice7 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a String");
-        String name=scanner.nextLine();
-        char character[]=name.toCharArray();
-        int length=name.length();
-
+        String name = scanner.nextLine();
+        char character[] = name.toCharArray();
+        int length = name.length();
+    }
+}
