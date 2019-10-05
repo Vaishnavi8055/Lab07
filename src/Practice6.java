7323e55bbcafc1730adb4243a49d8d54f53086af
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 09:24:10 -0700

    Enter a String

--- src/Practice6.java
+++ src/Practice6.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Practice6 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-
+System.out.println("Enter a String");
     }
 }
