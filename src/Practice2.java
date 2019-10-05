55bce11d0975cd3ea62af03145b3dc1b30daefb4
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 08:27:20 -0700

    String input from user

--- src/Practice2.java
+++ src/Practice2.java
@@ -3,6 +3,7 @@ public class Practice2 {
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
         System.out.println("Enter a String");
+        String inputString=scanner.nextLine();
     }
 
 }
