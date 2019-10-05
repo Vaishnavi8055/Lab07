8cd4bb1de1daac31d646d24e47054ce9a2507c8a
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 11:05:10 -0700

    index printed

--- src/Practiceindex2.java
+++ src/Practiceindex2.java
@@ -3,6 +3,16 @@ public class Practiceindex2 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("enter a string");
+        String str=sc.nextLine();
+        char array[]=str.toCharArray();
+        char ch='a';
+        for(int i=0;i<str.length();i++)
+        {
+            if(array[i]==ch){
+                System.out.println(i);
+            }
+        }
+
     }
 
 }
