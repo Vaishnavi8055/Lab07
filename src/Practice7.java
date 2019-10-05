ee5c10a868f70fa8bc670a972fffc99330fb4477
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2019-10-05 09:34:55 -0700

    input String

--- src/Practice7.java
+++ src/Practice7.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Practice7 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-
+            String name=scanner.nextLine();
     }
 
 }
